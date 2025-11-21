23b7c71494c9badbb3c5af30b2aeab835b61cca4
Debbie-alt <funmmy565@gmail.com>
2025-11-21 21:22:08 +0100

    Dependency update

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
 import './global.css';
+import {createStaticNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from 'pages/HomeScreen';
+import CheckMeals from 'pages/CheckMeals';
 
 
 export default function App() {
+
+  const RootStack = createNativeStackNavigator(
+    {
+      Home: {
+      screen: HomeScreen,
+      options: {title: 'Welcome'},
+    },
+    Onboarding: {
+      screen: CheckMeals,
+    },
+    }
+  );
+  const Navigation = createStaticNavigation(RootStack);
   return (
     <>
-      <ScreenContent  path="App.tsx"></ScreenContent>
+      <Navigation/>
       <StatusBar style="auto" />
     </>
   );
--- components/Onboarding/CheckMeals.tsx
+++ components/Onboarding/CheckMeals.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Button, View, SafeAreaViewBase, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
 
 const CheckMeals = () => {
+  const navigation = useNavigation();
   return (
-    <div>CheckMeals</div>
+    <SafeAreaViewBase className='flex-1'>
+    <View className='bg-red-500 h-1/2 w-full'>
+
+
+      <Button title="" onPress={() => alert('Button Pressed!')} />
+
+    </View>
+    <View className='bg-white h-1/2 w-full justify-center items-center py-7 text-black '>
+        <Text className='text-2xl font-medium mb-4'>Check for your meals</Text>
+        <Text className='text-center w-2/3'>Explore fresh and wholesome meals options
+          made just for you . Healthy eating doesn’t 
+          have to be complicated - we keep it
+          simple and satisfying</Text>
+        <Button title="Next" onPress={() => navigation.navigate('Home' as never)} />
+        <Button title="Skip" onPress={() => navigation.navigate('Home' as never)} />
+
+
+    </View>
+    </SafeAreaViewBase>
   )
 }
 
